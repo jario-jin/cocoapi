9f52a265f1b1765ed0417ebef59fa1756e7565cd
jario-jin <jariof@foxmail.com>
2019-01-18 18:35:53 +0800

    fix maxdet

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -458,18 +458,19 @@ class COCOeval:
             return mean_s
         def _summarizeDets():
             stats = np.zeros((12,))
-            stats[0] = _summarize(1)
-            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[0] = _summarize(1, maxDets=self.params.maxDets[-1])
+            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[-1])
+            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[-1])
+            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[-1])
+            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[-1])
+            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[-1])
             stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
             stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
             stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+            stats[8] = _summarize(0, maxDets=self.params.maxDets[-1])
+            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[-1])
+            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[-1])
+            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[-1])
             return stats
         def _summarizeKps():
             stats = np.zeros((10,))
@@ -527,7 +528,7 @@ class Params:
 
     def __init__(self, iouType='segm', maxDet=100):
         if iouType == 'segm' or iouType == 'bbox':
-            self.setDetParams()
+            self.setDetParams(maxDet=maxDet)
         elif iouType == 'keypoints':
             self.setKpParams()
         else:
